7844374471963d95ce6e6f8ac6ad87ee349ba567
tindaiz <101464909+tindaiz@users.noreply.github.com>
2024-12-19 22:15:08 +0700

    Delete 127.0.0.1.session.sql

--- 127.0.0.1.session.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/processed_data.csv'
-INTO TABLE hotel_details
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
